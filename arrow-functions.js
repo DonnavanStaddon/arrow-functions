e8cc07acde2130039534c95d68d946cfc0b3e1eb
Donnavan Stadd <donnystaddon@gmail.com>
2023-06-12 08:20:30 +0000

    arrow-Functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,29 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a, b) {
+// Code Block
+//return a + b;
+//}
+//let sum = addTwoNumbers(3, 5);
+//console.log(sum);
 
-// Arrow Function With Parameters
+// Arrow Function With Parameters ES6
+const addTwoNumbers = (a, b) => {
+    // Code Block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
-// Single Line Arrow Function With Parameters
+// Single Line Arrow Function With Parameters ES6 Shorter Code
+const addTwoNumbers3 = (a, b) => (a + b); //also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
-// Implicit Returns
+// Implicit Returns = for simpler functions such as those that only take a single parameter
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
 
 // Returning Multiple Lines
